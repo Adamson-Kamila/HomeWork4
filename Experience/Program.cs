a7c144157b3f0b39a1f0991493b08d91b1b1cff8
Kamila <kamilla.adamson@gmail.com>
2023-02-09 23:52:09 +0600

    Added a Task41

---
+++ Experience/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Clear();
+
+int [,] myArray = new int [,]
+{
+    {10, 4, 5},
+    {7, 4, 8},
+    {4, 10, 2}
+};
+
+// foreach (var item in myArray) //цикл foreach позволяет просто и быстро вывести элемента массива в консоль.
+// // задача этого цикла заключается в переборе элементов в коллекции. Этот цикл будет пробегаться по всем элементам 
+// // массива и извлекать их. Пока цикл будет пробегать по всем эементам массива, внутри тела foreach
+// // команда ConsoleWritLine будет выводить их по порядку в консоль. 
+// {
+//     Console.WriteLine(item); //WriteLine - вертикальный вывод. Write - горизонтальный. Для добавдения
+//     // пробела между эементам  - (item + " ");
+// }
+
+//Console.Write(myArray.Rank); //Rank - оператор, отображающий количество измерений массива. В нашем случае это 2.
+//Console.Write(myArray.Length); //Length - оператор, указывающий длину массива и как следствие количество элементов.
+// В нашем случае их - 9.
+//Console.Write(myArray.GetLength);
+
+//Console.WriteLine(myArray.GetLength(1)); //GetLength позволит увидеть сколько строк и колонок в массиве, если укажем 0
+// то покажет строки, в нашем случае это - 3, если 1, то покажет количество колонок, в нашем случае - это 3.
+
+//int heigth  = myArray.GetLength(0);
+//int width = myArray.GetLength(1);
+
+for (int y = 0; y < myArray.GetLength(0); y++)
+{
+    Console.Write(myArray[2,y] +"\t"); // 2 указывает индекс строки
+}
+
+for (int x = myArray.GetLength(1)-2; x >= 0; x--)
+{
+    Console.Write(myArray[x,2] +"\t");
+}
+
+for (int y = myArray.GetLength(0)-2; y >= 0 ; y--)
+{
+    Console.Write(myArray[0,y] +"\t");
+}
+
+for (int y = 0; y < myArray.GetLength(0)-1; y++)
+{
+    Console.Write(myArray[1,y] +"\t");
+}
+
+// {
+// for (int y = 0; y < myArray.GetLength(1); y++)
+//     {
+//          Console.Write(myArray[x,y] +"\t");
+//     }
+//      Console.WriteLine();
+// }
---
+++ Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры М чисе. Посчитать сколько чисел больше 0 ввел пользователь.
+//Например: 0, 7, 8, -2, -2 -> 2 или 1, -7, 567, 89, 223 -> 4
+
+
+int[] youarray ()
+{
+    Console.Clear();
+    Console.Write("Введите элементы массива\t");
+    int[]myArray = new int [5];
+
+    for(int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"\nВведите элемент массива под индексом {i}:\t ");
+    myArray[i] = int.Parse(Console.ReadLine()!);
+    
+}
+    return myArray;
+}
+
+
+int quantity(int [] myArray)
+{
+    int quantity = 0;
+    for(int i = 0; i < myArray.Length; i++)
+{
+    if (myArray [i] > 0)
+    quantity +=1;
+
+}
+    return quantity;
+    //Console.WriteLine(quantity);
+}
+
+int [] array = youarray();
+int num = quantity(array);
+Console.Write("[{0}]", string.Join(",", array ));
+Console.Write("->" );
+Console.Write(num );
+
